06b8c46a7cc6fca86085985003fa81bd0e73ec9b
tamarajafar <117233941+tamarajafar@users.noreply.github.com>
2023-11-02 12:47:42 -0700

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,7 +4,7 @@ export default {
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://www.tamarajafar.co/",
+    url: "tamarajafar.github.io",
     site_name: "Tamara Jafar",
   },
   twitter: {
